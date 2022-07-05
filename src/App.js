6d809d07d791332dd2edf09385d0fd53ca39b25c
Mahbub Noyon <mahbubnoyon506@gmail.com>
2022-07-05 20:04:01 +0600

    Homepage complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import 'tw-elements';
 import Home from './Pages/Home';
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,48 +1,66 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from 'react-query'
 import art from '../Assests/Images/image1.png'
 import art2 from '../Assests/Images/image2.png'
 import art3 from '../Assests/Images/image3.png'
 import Loader from '../Components/Loader';
+import { FaRegQuestionCircle } from "react-icons/fa";
+import { FaAngleRight } from "react-icons/fa";
+import { FaRegHeart } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import { RiShareForwardLine } from "react-icons/ri";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
 
 const Home = () => {
 
-    const {  data, isLoading} = useQuery('classdata', () =>
+    useEffect( () => {
+        AOS.init({
+            duration: 1000,
+        });
+    }, [])
+
+    const { data, isLoading } = useQuery('classdata', () =>
         fetch('classData.json').then(res =>
             res.json()
         )
     )
-    if(isLoading){
-      return <Loader></Loader>
+    if (isLoading) {
+        return <Loader></Loader>
     }
-    console.log(data)
     return (
-        <div>
+        <div className='p-10'>
             {
                 data.map(d =>
                     <div key={d.id}>
-                        <h1>{d.title}</h1>
-                        <h3>{d.classeType}</h3>
-                        <div className='md:grid gap-10 grid-cols-2'>
-                            <div>
-                                <p>{d.description}</p>
-                                <div><img src={d.teacherImage} alt="" /> <h2 className='text-[#503DD4]'>{d.teacherName}</h2></div>
-                                <p>{d.teacherReview}</p>
-                                <p>{d.classReview}</p>
-                                <p>{d.completedStudent}</p>
-                                <div>
-                                    <button type="button" class="inline-block px-6 py-2.5 bg-[#503DD4] text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out">Secondary</button>
-                                    <p className='text-[#503DD4]'>Save</p>
-                                    <p className='text-[#503DD4]'>Share</p>
+                        <h1 className='text-5xl font-bold leading-tight'>{d.title}</h1>
+                        <h3 className='text-lg font-semibold text-[#808080] mr-2 inline-flex items-center'>{d.classeType} <span className='ml-1'><FaRegQuestionCircle /></span></h3>
+                        <div className='lg:grid gap-10 grid-cols-2 mt-16'>
+                            <div className='' data-aos="fade-right">
+                                <p className='text-lg leading-normal'>{d.description}</p>
+                                <div className='flex items-center my-5'><img className="rounded-full w-12 mr-3" src={d.teacherImage} alt="" /> <h2 className='text-lg font-semibold text-[#503DD4]'>{d.teacherName}</h2></div>
+                                <p className='text-sm text-[#808080] inline-flex items-center'> <span className='text-[#F9C847] inline-flex mr-1'> <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /> </span> {d.teacherReview} Total reviews for this teacher.</p>
+                                <p className='text-sm text-[#808080] my-2'> <span className='text-[#F9C847] inline-flex mr-1'> <FaStar /> <FaStar /> <FaStar /> <FaStar /> <FaStar /> </span> {d.classReview} Total reviews for this class.</p>
+                                <p className='text-xl'>Completed by {d.completedStudent} Learners.</p>
+                                <div className='flex items-center my-8'>
+                                    <button type="button" className="px-6 py-3.5 text-lg bg-[#503DD4] text-white font-medium leading-tight rounded-full shadow-md hover:bg-purple-700 hover:shadow-lg focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-purple-800 active:shadow-lg transition duration-150 ease-in-out tracking-wide inline-flex items-center">See Class Schedule <span><FaAngleRight /></span> </button>
+                                    <button className='text-lg font-semibold text-[#503DD4] inline-flex items-center hover:text-purple-700 mx-8'> <span className='mr-1'><FaRegHeart /></span> Save</button>
+                                    <button className='text-lg font-semibold text-[#503DD4] inline-flex items-center hover:text-purple-700'> <span className='mr-1'> <RiShareForwardLine /> </span> Share</button>
                                 </div>
                             </div>
-                            <div className='flex'>
-                                <div className='mr-3'>
-                                    <img className='w-full' src={art} alt="" />
+                            <div className='md:flex object-cover' >
+                                <div className="relative overflow-hidden bg-no-repeat bg-cover max-w-xs mr-2" data-aos="zoom-in">
+                                    <img src={art} className="max-w-xs hover:scale-110 transition duration-300 ease-in-out h-full" alt="Louvre" />
                                 </div>
                                 <div>
-                                    <img className='w-full mb-1 pb-2' src={art2} alt="" />
-                                    <img className='w-full' src={art3} alt="" />
+                                    <div className="relative overflow-hidden bg-no-repeat bg-cover mb-2" data-aos="zoom-in">
+                                        <img src={art2} className=" hover:scale-110 transition duration-300 ease-in-out h-full w-full" alt="Louvre" />
+                                    </div>
+                                    <div className="relative overflow-hidden bg-no-repeat bg-cover" data-aos="zoom-in">
+                                        <img src={art3} className=" hover:scale-110 transition duration-300 ease-in-out h-full w-full" alt="Louvre" />
+                                    </div>
                                 </div>
                             </div>
                         </div>
